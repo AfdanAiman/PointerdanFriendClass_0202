296c785f1f7b61c72c4422974213a2c696567901
AfdanAiman <afdanaiman06@gmail.com>
2025-05-05 16:33:17 +0700

    menambahkan int main

--- pinterSatu.cpp
+++ pinterSatu.cpp
@@ -9,6 +9,20 @@ class mahasiswa{
     }
 };
 
+int main(){
+    mahasiswa mhs; // object mhs
+    mhs.nim = 5;
+    mhs.showNim(); //Member acces 
+    
+    mahasiswa &refmhs = mhs; // pointer references refmhs
+    refmhs.nim = 2; //Member access Operator
+    mhs.showNim();
+
+    mahasiswa *pmhs = &mhs; //pointer Dereference pmhs
+    pmhs->nim = 3; //Arrow Operator
+    pmhs->showNim();
+    return 0;
+}
 
 
     
